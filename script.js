f3d1dc62cad8ee16aad508e9ddeeb52aa3c60032
SindhubairaviNirmala <sindhubairavi922@gmail.com>
2024-11-26 21:15:54 +0530

    reupdated

--- script.js
+++ script.js
@@ -26,3 +26,180 @@ function showshow(){
     hide1.style.display="block"
     second.style.display="none"
  }
+ 
+ var third=document.getElementById("third")
+ var hide2=document.getElementById("hide2")
+ 
+ function showA(){
+     hide2.style.display="block"
+     third.style.display="none"
+  }
+ 
+  var fourth=document.getElementById("fourth")
+var hide3=document.getElementById("hide3")
+
+function showB(){
+    hide3.style.display="block"
+    fourth.style.display="none"
+ }
+ 
+ var fifth=document.getElementById("fifth")
+var hide4=document.getElementById("hide4")
+
+function showC(){
+    hide4.style.display="block"
+    fifth.style.display="none"
+ }
+ 
+ var sixth=document.getElementById("sixth")
+var hide5=document.getElementById("hide5")
+
+function showD(){
+    hide5.style.display="block"
+    sixth.style.display="none"
+ }
+ 
+ var seventh=document.getElementById("seventh")
+var hide6=document.getElementById("hide6")
+
+function showE(){
+    hide6.style.display="block"
+    seventh.style.display="none"
+ }
+ 
+ var eight=document.getElementById("eight")
+var hide7=document.getElementById("hide7")
+
+function showF(){
+    hide7.style.display="block"
+    eight.style.display="none"
+ }
+ 
+ var nineth=document.getElementById("nineth")
+var hide8=document.getElementById("hide8")
+
+function showG(){
+    hide8.style.display="block"
+    nineth.style.display="none"
+ }
+ 
+ var tenth=document.getElementById("tenth")
+var hide9=document.getElementById("hide9")
+
+function showH(){
+    hide9.style.display="block"
+    tenth.style.display="none"
+ }
+ 
+ var eleventh=document.getElementById("eleventh")
+var hide10=document.getElementById("hide10")
+
+function showI(){
+    hide10.style.display="block"
+    eleventh.style.display="none"
+ }
+ 
+ var twelve=document.getElementById("twelve")
+var hide11=document.getElementById("hide11")
+
+function showJ(){
+    hide11.style.display="block"
+    twelve.style.display="none"
+ }
+ 
+ var thirteen=document.getElementById("thirteen")
+var hide12=document.getElementById("hide12")
+
+function showK(){
+    hide12.style.display="block"
+    thirteen.style.display="none"
+ }
+ 
+ var fourteen=document.getElementById("fourteen")
+var hide13=document.getElementById("hide13")
+
+function showL(){
+    hide13.style.display="block"
+    fourteen.style.display="none"
+ }
+ 
+ var fifteen=document.getElementById("fifteen")
+var hide14=document.getElementById("hide14")
+
+function showM(){
+    hide14.style.display="block"
+    fifteen.style.display="none"
+ }
+ 
+ var sixteen=document.getElementById("sixteen")
+var hide15=document.getElementById("hide15")
+
+function showN(){
+    hide15.style.display="block"
+    sixteen.style.display="none"
+ }
+ 
+ var seventeen=document.getElementById("seventeen")
+var hide16=document.getElementById("hide16")
+
+function showO(){
+    hide16.style.display="block"
+    seventeen.style.display="none"
+ }
+ 
+ var eighteen=document.getElementById("eighteen")
+var hide17=document.getElementById("hide17")
+
+function showP(){
+    hide17.style.display="block"
+    eighteen.style.display="none"
+ }
+ 
+ var nineteen=document.getElementById("nineteen")
+var hide18=document.getElementById("hide18")
+
+function showQ(){
+    hide18.style.display="block"
+    nineteen.style.display="none"
+ }
+ 
+ var twenty=document.getElementById("twenty")
+var hide19=document.getElementById("hide19")
+
+function showR(){
+    hide19.style.display="block"
+    twenty.style.display="none"
+ }
+ 
+ var twentyone=document.getElementById("twentyone")
+var hide20=document.getElementById("hide20")
+
+function showS(){
+    hide20.style.display="block"
+    twentyone.style.display="none"
+ }
+ 
+ var twentytwo=document.getElementById("twentytwo")
+var hide21=document.getElementById("hide21")
+
+function showT(){
+    hide21.style.display="block"
+    twentytwo.style.display="none"
+ }
+ 
+ var twentythree=document.getElementById("twentythree")
+var hide22=document.getElementById("hide22")
+
+function showU(){
+    hide22.style.display="block"
+    twentythree.style.display="none"
+ }
+ 
+ var twentyfour=document.getElementById("twentyfour")
+var hide23=document.getElementById("hide23")
+
+function showV(){
+    hide23.style.display="block"
+    twentyfour.style.display="none"
+ }
+ 
