f5740b748221f3d8e267414fa20e17aa1fc318e0
SindhubairaviNirmala <sindhubairavi922@gmail.com>
2024-11-26 10:39:38 +0530

    reupdated

--- script.js
+++ script.js
@@ -18,3 +18,11 @@ function show(){
         hide.style.display="block"
         first.style.display="none"
 } 
+
+var second=document.getElementById("second")
+var hide1=document.getElementById("hide1")
+
+function showshow(){
+    hide1.style.display="block"
+    second.style.display="none"
+ }
